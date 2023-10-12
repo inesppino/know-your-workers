5b237cf9cd915dbccdd7e429d508181798fc4d23
inesppino <inesp.pino@gmail.com>
2023-10-12 13:06:30 +0200

    :sparkles: create table with mocked data

--- frontend/src/WorkersTable.jsx
+++ frontend/src/WorkersTable.jsx
@@ -1,3 +1,48 @@
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
+
 export default function WorkersTable() {
-  return <div>Tabla</div>;
+  const workers = [
+    { name: "Inés", area: "Dev", projects: ["SO69"], status: "ok", id: 1 },
+    {
+      name: "Enrique",
+      area: "Dev",
+      projects: ["SO69", "Metrodora"],
+      status: "ok",
+      id: 2,
+    },
+  ];
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Nombre</Th>
+            <Th>Disciplina</Th>
+            <Th>Proyectos</Th>
+            <Th>Estado</Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {workers.map((worker) => (
+            <Tr key={worker.id}>
+              <Td>{worker.name}</Td>
+              <Td>{worker.area}</Td>
+              <Td>{worker.projects.join(", ")}</Td>
+              <Td>{worker.status}</Td>
+              <Td> | </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
 }
