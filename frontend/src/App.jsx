a2fe9e1c4fcba7ab411bb1849ab8a680a8f5f428
inesppino <inesp.pino@gmail.com>
2023-10-14 15:05:42 +0200

    :sparkles: create resetCSS file and pass down the theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,27 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 import Login from "./Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { customTheme } from "./assets/reset";
 import WorkersTable from "./WorkersTable";
+import MainLayout from "./components/layout/MainLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/trabajadores",
-    element: <WorkersTable />,
+    element: <MainLayout />,
+    children: [
+      { path: "login", element: <Login /> },
+      {
+        path: "/trabajadores",
+        element: <WorkersTable />,
+      },
+    ],
   },
 ]);
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <RouterProvider router={router} />
     </ChakraProvider>
   );
