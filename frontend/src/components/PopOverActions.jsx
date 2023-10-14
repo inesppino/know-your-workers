fe19f8a00882a85972974d4d2bed7cf281618fa3
inesppino <inesp.pino@gmail.com>
2023-10-14 14:55:22 +0200

    :sparkles: create popover component for actions in table

---
+++ frontend/src/components/PopOverActions.jsx
@@ -0,0 +1,44 @@
+import {
+  Button,
+  ButtonGroup,
+  Icon,
+  Popover,
+  PopoverArrow,
+  PopoverContent,
+  PopoverTrigger,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { IoMdMore } from "react-icons/io";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { MdOutlineEdit } from "react-icons/md";
+
+export default function PopOverActions() {
+  const { isOpen, onToggle, onClose } = useDisclosure();
+
+  return (
+    <Popover
+      returnFocusOnClose={false}
+      isOpen={isOpen}
+      onClose={onClose}
+      placement="right"
+      closeOnBlur={false}
+    >
+      <PopoverTrigger>
+        <Button onClick={onToggle}>
+          <Icon as={IoMdMore} />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent sx={{ width: "auto" }}>
+        <PopoverArrow />
+        <ButtonGroup display="flex" flexDirection="column" gap={0}>
+          <Button variant="ghost" leftIcon={<Icon as={MdOutlineEdit} />}>
+            Editar
+          </Button>
+          <Button variant="ghost" leftIcon={<Icon as={RiDeleteBin6Line} />}>
+            Borrar
+          </Button>
+        </ButtonGroup>
+      </PopoverContent>
+    </Popover>
+  );
+}
